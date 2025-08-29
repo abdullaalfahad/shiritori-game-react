11c6fffbd1adf9d07b7eaeaeeca9428f4f761892
abdullaalfahad <aafahad02@gmail.com>
2025-08-29 20:53:30 +0600

    chore: setup React + Vite project with Tailwind, ESLint, and Prettier

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tailwind from 'eslint-plugin-tailwindcss';
+import prettier from 'eslint-config-prettier';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      tailwind.configs.recommended,
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      tailwind,
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+]);
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-blue-400">Initial Setup</h1>;
+}
+
+export default App;
